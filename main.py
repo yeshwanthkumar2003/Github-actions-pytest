179f4e6c410406cc14d525b0742640084ea97821
yeshwanth <94180456+yeshwanthkumar2003@users.noreply.github.com>
2022-10-23 22:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ print("CI CD Integration with github actions")
 
 
 def welcome():
-  print("I am yesh")
+    print("I am yesh")
+    
 
-  
-  print("This is CI/CD with python")
-  welcome()
+print("This is CI/CD with python")
+welcome()
